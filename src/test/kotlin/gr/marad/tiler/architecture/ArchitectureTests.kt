e03791b12ba67b075ed7032984b30b53c7392d68
Marcin Radoszewski <moriturius@gmail.com>
2023-07-27 11:35:51 +0200

    Add checking blocks cyclic dependencies

--- src/test/kotlin/gr/marad/tiler/architecture/ArchitectureTests.kt
+++ src/test/kotlin/gr/marad/tiler/architecture/ArchitectureTests.kt
@@ -6,22 +6,22 @@ import com.tngtech.archunit.core.domain.JavaClasses
 import com.tngtech.archunit.core.domain.JavaPackage
 import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTest
-import com.tngtech.archunit.lang.ArchRule
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import com.tngtech.archunit.lang.*
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.all
 import com.tngtech.archunit.library.GeneralCodingRules
+import kotlin.jvm.optionals.getOrNull
 
 @AnalyzeClasses(packages = ["gh.marad.tiler.."])
 class ArchitectureTests {
+    private val blocks = BlockTransformer()
+    private val beFreeOfCycles = BlocksFreeOfCyclesCondition()
+    private val restrictAccessToInternalClasses = BlocksRestrictAccessToInternalClassesCondition()
 
     @ArchTest
-    fun `should not include internal classes of other modules`(classes: JavaClasses) {
-        getModules(classes).forEach { modulePackage ->
-            val rule = classes().that().resideInAPackage("${modulePackage.name}.internal..")
-                .should().onlyBeAccessed().byClassesThat().resideInAPackage("${modulePackage.name}..")
+    val blocksRestrictAccessToInternalClasses: ArchRule = all(blocks).should(restrictAccessToInternalClasses)
 
-            rule.check(classes)
-        }
-    }
+    @ArchTest
+    val blocksShouldBeFreeOfCycles: ArchRule = all(blocks).should(beFreeOfCycles)
 
     @ArchTest
     val noGenericExceptions: ArchRule = GeneralCodingRules.NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS
@@ -38,11 +38,101 @@ class ArchitectureTests {
     @ArchTest
     val noUsageOfStdStreams: ArchRule = GeneralCodingRules.NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS
 
-
     private fun getModules(classes: JavaClasses): List<JavaPackage> {
         val areInternalClasses = object : DescribedPredicate<JavaClass>("are internal classes") {
             override fun test(t: JavaClass): Boolean = t.packageName.endsWith(".internal")
         }
         return classes.that(areInternalClasses).map { it.`package`.parent.get() }.distinct()
+    }
+}
+
+class BlocksFreeOfCyclesCondition : ArchCondition<Block>("be free of cycles") {
+    override fun check(block: Block, events: ConditionEvents) {
+        val queue = ArrayDeque<List<Block>>()
+        queue.addFirst(listOf(block))
+
+        while (true) {
+            val currentPath = queue.removeFirstOrNull() ?: break
+
+            if (currentPath.size > currentPath.distinctBy { it.javaPackage.name }.size) {
+                events.add(SimpleConditionEvent.violated(currentPath, "dependency cycle detected: ${currentPath.map { it.javaPackage.name }}"))
+                break
+            }
+
+            currentPath.last().dependenciesToThisBlock().forEach {
+                queue.add(currentPath.plus(it.origin))
+            }
+        }
+    }
+}
+
+class BlocksRestrictAccessToInternalClassesCondition : ArchCondition<Block>("restrict access to internal classes") {
+    override fun check(item: Block, events: ConditionEvents) {
+        val blockClasses = item.javaPackage.classes
+        item.internalPackage().classDependenciesToThisPackageTree.forEach {
+            if(!blockClasses.contains(it.originClass)) {
+                events.add(SimpleConditionEvent.violated(it, it.description))
+            }
+        }
+    }
+
+}
+
+data class Block(val javaPackage: JavaPackage) {
+    val blockParentPackage = javaPackage.parent.get()
+
+    fun internalClasses(): Set<JavaClass> = internalPackage().classesInPackageTree
+
+    fun internalPackage() = println(javaPackage).let { javaPackage.getPackage("internal") }
+
+    fun dependenciesFromThisBlock(): Set<BlockDependency> {
+        return javaPackage.classDependenciesFromThisPackageTree
+            .asSequence()
+            .mapNotNull { findBlockPackage(it.targetClass.`package`) }.distinct()
+            .map { BlockDependency(this, Block(it)) }
+            .toSet()
+    }
+
+    fun dependenciesToThisBlock(): Set<BlockDependency> {
+        return javaPackage.classDependenciesToThisPackageTree
+            .asSequence()
+            .mapNotNull { findBlockPackage(it.originClass.`package`) }.distinct()
+            .map { BlockDependency(Block(it), this) }
+            .toSet()
+    }
+
+    private fun isMainBlockPackage(javaPackage: JavaPackage): Boolean =
+        javaPackage.subpackages.any { it.relativeName == "internal" }
+                && javaPackage.classes.any { it.simpleName.endsWith("Facade") }
+
+    /// Returns block root package or null if given package does not belong to a block.
+    fun findBlockPackage(javaPackage: JavaPackage): JavaPackage? {
+        if (isMainBlockPackage(javaPackage)) return javaPackage
+        if (!javaPackage.name.contains(".internal")) return null
+        var currentPackage = javaPackage
+        while(true) {
+            if (currentPackage.name.endsWith(".internal")) break
+            currentPackage = currentPackage.parent.getOrNull() ?: return null
+        }
+        val potentialBlockPackage = currentPackage.parent.getOrNull() ?: return null
+        if (isMainBlockPackage(potentialBlockPackage)) return potentialBlockPackage
+        return null
+    }
+}
+
+data class BlockDependency(val origin: Block, val target: Block)
+
+class BlockTransformer : AbstractClassesTransformer<Block>("blocks") {
+    private val areInternalClasses = object : DescribedPredicate<JavaClass>("are internal classes") {
+        override fun test(t: JavaClass): Boolean = t.packageName.endsWith(".internal")
+    }
+
+    override fun doTransform(classes: JavaClasses): MutableIterable<Block> {
+        val blockPackages = classes.that(areInternalClasses)
+            .map { it.`package`.parent.get() }.distinct().map { it.name }
+
+        return blockPackages.map {
+             Block(classes.getPackage(it))
+        }.toMutableSet()
     }
 }
